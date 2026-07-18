3595bdf9ee96d923228817f1dc801edecb833e01
Anmol kumar <anm65642@gmail.com>
2026-07-18 22:54:35 +0530

    Update embedding.py

--- backend/rag/embedding.py
+++ backend/rag/embedding.py
@@ -1,16 +1,36 @@
+import os
 import faiss
 import numpy as np
-from sentence_transformers import SentenceTransformer
+from openai import OpenAI
 
-embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+client = OpenAI(
+    api_key=os.getenv("JINA_API_KEY"),
+    base_url="https://api.jina.ai/v1"
+)
+
+def get_embeddings(texts):
+    response = client.embeddings.create(
+        model="jina-embeddings-v4",
+        input=texts
+    )
+
+    embeddings = np.array(
+        [item.embedding for item in response.data],
+        dtype=np.float32
+    )
+
+    return embeddings
+
 
 def build_index(documents):
     texts = [doc["content"] for doc in documents]
 
-    embeddings = embedding_model.encode(texts, convert_to_numpy=True)
-    embeddings = embeddings.astype(np.float32)
+    embeddings = get_embeddings(texts)
 
     dimension = embeddings.shape[1]
+
     index = faiss.IndexFlatL2(dimension)
+
     index.add(embeddings)
+
     return index
